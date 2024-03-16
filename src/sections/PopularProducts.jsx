1a21f7e18905bd43fd25ff5a6d5664d2511bebdd
Vineet <infinite2202@gmail.com>
2024-03-16 21:06:45 +0530

    nike

--- src/sections/PopularProducts.jsx
+++ src/sections/PopularProducts.jsx
@@ -16,7 +16,7 @@ const PopularProducts = () => {
         </p>
       </div>
 
-      <div className="mt-16 grid lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 grid-cols-1 sm:gap-6 gap-14">
+      <div className="mt-16 grid lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 grid-cols-1 sm:gap-6 gap-14 cursor-pointer">
         {products.map((product) => (
           <PopularPoductCard key={product.name} {...product} />
         ))}
